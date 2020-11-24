615cfe156b2b1b21872bd3eed462b06f8cd0ac88
jeannillehici <hicianoj@northeastern.edu>
2020-11-24 15:10:04 -0500

    updated server.js for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/myapp'));
+app.use(express.static(__dirname + '/dist/cs5610-f20-hicianoj-client-angular-2'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
                          '/dist/cs5610-f20-hicianoj-client-angular-2/index.html'));});
